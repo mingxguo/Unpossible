7ce00fbb9a4a74ffac2f91de848b5b82e6aace38
Mingxiao Guo <mingxiao.guo.chen@gmail.com>
2019-12-10 23:48:28 +0100

    Add menus

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject SettingsMenu;
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Settings()
+    {
+        gameObject.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class SettingsMenu : MonoBehaviour
+{
+
+    private Slider player_speed_slider;
+    private Slider rotate_speed_slider;
+
+    private void OnEnable()
+    {
+        Slider[] sliders = gameObject.GetComponentsInChildren<Slider>();
+        Debug.Log("start");
+        foreach (Slider s in sliders)
+        {
+            if (s.name == "PlayerSpeed")
+            {
+                player_speed_slider = s;
+                Debug.Log("Found " + s.name);
+            }
+            else
+            {
+                rotate_speed_slider = s;
+                Debug.Log("Found " + s.name);
+            }
+        }
+    }
+
+    public void SetPlayerSpeed()
+    {
+        Debug.Log("player speed" + player_speed_slider.value);
+        Follower.PlayerSpeed = player_speed_slider.value;
+    }
+
+    public void SetRotateSpeed()
+    {
+
+        Debug.Log("rotate speed" + rotate_speed_slider.value);
+        CameraRotator.RotateSpeed = rotate_speed_slider.value;
+    }
+}
